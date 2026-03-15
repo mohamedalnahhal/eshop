7c84d64918e2aaf49a6f1a91103cfeae162725ee
moh123 <king0abusaleh@gmail.com>
2026-03-15 12:10:43 +0200

    add customization vars

--- src/routes/tenant.php
+++ src/routes/tenant.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
-
+use App\Livewire\Tenant\HomePage;
 /*
 |--------------------------------------------------------------------------
 | Tenant Routes
@@ -23,7 +23,5 @@ Route::middleware([
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
-    Route::get('/', function () {
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
-    });
+    Route::get('/', HomePage::class)->name('tenant.home');
 });
