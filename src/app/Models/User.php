2547bee6e0205847f5c96847deb3e3b13ad5c6b5
mohamedalnahhal <anahallmohammed@gmail.com>
2026-03-27 18:51:07 +0200

    Database: add avatar to users table

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements FilamentUser
 {
     use HasUuids, Notifiable;
 
-    protected $fillable = ['name', 'username', 'email', 'password', 'phone', 'gender', 'role'];
+    protected $fillable = ['name', 'username', 'email', 'avatar', 'password', 'phone', 'gender', 'role'];
 
     protected $hidden = ['password', 'remember_token'];
 
@@ -28,6 +28,13 @@ class User extends Authenticatable implements FilamentUser
         'role' => UserRole::class,
         'gender' => Gender::class,
     ];
+
+    public function getAvatarUrlAttribute(): string
+    {
+        return $this->avatar
+            ? asset('storage/' . $this->avatar)
+            : 'https://ui-avatars.com/api/?name=' . urlencode($this->name) . '&background=2563eb&color=fff&bold=true';
+    }
 
     public function tenants(): BelongsToMany
     {
