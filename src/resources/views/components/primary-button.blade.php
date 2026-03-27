fbaa0bf1403c8f136824729624d6218709d1c0be
mohamedalnahhal <anahallmohammed@gmail.com>
2026-03-27 18:58:06 +0200

    update primary-button component

--- src/resources/views/components/primary-button.blade.php
+++ src/resources/views/components/primary-button.blade.php
@@ -3,7 +3,7 @@
 <button 
     {{ $attributes->merge([
         'type' => $type, 
-        'class' => 'w-full bg-blue-600 text-white text-md font-bold py-2 rounded-lg hover:bg-blue-700 transition shadow-xl shadow-blue-100 active:scale-95 disabled:opacity-70 disabled:cursor-not-allowed flex justify-center items-center gap-2'
+        'class' => 'w-full bg-blue-600 text-white text-md font-bold py-2 rounded-lg hover:bg-blue-700 transition not-disabled:shadow-xl shadow-blue-100 active:scale-95 disabled:bg-gray-300 disabled:text-gray-700 disabled:cursor-not-allowed flex justify-center items-center gap-2'
     ]) }}
 >
     {{ $slot }}
