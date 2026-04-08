2b18fa24cb2fa7d42a4c2b0b9f5a5a1b1bfe9bc7
mohamedalnahhal <anahallmohammed@gmail.com>
2026-04-08 13:06:52 +0300

    allow tenant-scoped themes alongside gloable themes

--- src/app/Models/Theme.php
+++ src/app/Models/Theme.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
 
 class Theme extends Model
 {
     use HasUuids;
+    use BelongsToTenant;
 
     protected $fillable = [
         'name',
