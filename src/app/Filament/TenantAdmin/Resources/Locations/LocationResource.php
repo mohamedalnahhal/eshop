7a2e02e22b336553c14118abe92f6ee0b4b81875
mohamedalnahhal <anahallmohammed@gmail.com>
2026-04-09 23:53:35 +0300

    Fix recordTitleAttribute in Location and Order resources in tenant panel

--- src/app/Filament/TenantAdmin/Resources/Locations/LocationResource.php
+++ src/app/Filament/TenantAdmin/Resources/Locations/LocationResource.php
@@ -20,7 +20,7 @@ class LocationResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
-    protected static ?string $recordTitleAttribute = 'Location';
+    protected static ?string $recordTitleAttribute = 'name';
 
     public static function form(Schema $schema): Schema
     {
--- src/app/Filament/TenantAdmin/Resources/Orders/OrderResource.php
+++ src/app/Filament/TenantAdmin/Resources/Orders/OrderResource.php
@@ -22,8 +22,6 @@ class OrderResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
-    protected static ?string $recordTitleAttribute = 'name';
-
     public static function form(Schema $schema): Schema
     {
         return OrderForm::configure($schema);
