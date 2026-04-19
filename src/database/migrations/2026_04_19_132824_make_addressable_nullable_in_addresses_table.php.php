03cd8adc942a58e02ee645ce52cb1a989ea4038d
mohammed-w-mosabeh <mohammed.w.mosabeh@gmail.com>
2026-04-19 16:51:59 +0300

    solve address problem

---
+++ src/database/migrations/2026_04_19_132824_make_addressable_nullable_in_addresses_table.php.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->string('addressable_type')->nullable()->change();
+            $table->uuid('addressable_id')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->string('addressable_type')->nullable(false)->change();
+            $table->uuid('addressable_id')->nullable(false)->change();
+        });
+    }
+};
