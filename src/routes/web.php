b85982da83cf211fc6819e65773125e5c4627012
mohamedalnahhal <anahallmohammed@gmail.com>
2026-04-23 19:26:34 +0300

    Fix: isolate central domains in web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,13 +12,16 @@ Route::get('/mock-checkout2', function () {
     return view('mock-checkout2');
 });
 
-// Landing
-Route::get('/', [LandingController::class, 'index'])->name('landing');
-Route::get('/eshop', [LandingController::class, 'index']);
-Route::get('lang/{locale}', [LandingController::class, 'switchLang'])->name('lang.switch');
-
-// Merchant auth (central domain)
-    Route::get('/login',    [MerchantLoginController::class,   'create'])->name('merchant.login');
-    Route::post('/login',   [MerchantLoginController::class,   'store'])->name('merchant.login.store');
-    Route::get('/register', [MerchantRegisterController::class, 'create'])->name('merchant.register');
-    Route::post('/register',[MerchantRegisterController::class, 'store'])->name('merchant.register.store');
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        // Landing
+        Route::get('/', [LandingController::class, 'index']);
+        Route::get('lang/{locale}', [LandingController::class, 'switchLang'])->name('lang.switch');
+    
+        // Merchant auth (central domain)
+        Route::get('/login',    [MerchantLoginController::class,   'create'])->name('merchant.login');
+        Route::post('/login',   [MerchantLoginController::class,   'store'])->name('merchant.login.store');
+        Route::get('/register', [MerchantRegisterController::class, 'create'])->name('merchant.register');
+        Route::post('/register',[MerchantRegisterController::class, 'store'])->name('merchant.register.store');
+    });
+}
