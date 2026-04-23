544e593c2c54aa747ff974457593ffcf314008a1
mohammed-w-mosabeh <mohammed.w.mosabeh@gmail.com>
2026-04-23 14:37:30 +0300

    update route to by add login & register

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,3 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth\Merchant\MerchantLoginController;
+use App\Http\Controllers\Auth\Merchant\MerchantRegisterController;
+use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
+
+// Landing
+Route::get('/', [LandingController::class, 'index'])->name('landing');
+Route::get('/eshop', [LandingController::class, 'index']);
+Route::get('lang/{locale}', [LandingController::class, 'switchLang'])->name('lang.switch');
+
+// Merchant auth (central domain)
+    Route::get('/login',    [MerchantLoginController::class,   'create'])->name('merchant.login');
+    Route::post('/login',   [MerchantLoginController::class,   'store'])->name('merchant.login.store');
+    Route::get('/register', [MerchantRegisterController::class, 'create'])->name('merchant.register');
+    Route::post('/register',[MerchantRegisterController::class, 'store'])->name('merchant.register.store');
+
